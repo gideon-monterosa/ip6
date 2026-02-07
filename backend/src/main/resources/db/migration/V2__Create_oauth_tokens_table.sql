7b5e139fa8e7c93bfc2bd99c1806059b50e61ef6
xenoisenegger <113588584+Xeen00@users.noreply.github.com>
2026-02-07 14:55:44 +0100

    GoogleCalendarApi: V2__Create_oauth_tokens_table.sql: add field for provider type

--- backend/src/main/resources/db/migration/V2__Create_oauth_tokens_table.sql
+++ backend/src/main/resources/db/migration/V2__Create_oauth_tokens_table.sql
@@ -1,10 +1,13 @@
 CREATE TABLE user_oauth_tokens (
                                    id BIGSERIAL PRIMARY KEY,
-                                   user_id BIGINT NOT NULL UNIQUE,
+                                   user_id BIGINT NOT NULL,
+                                   provider VARCHAR(20) NOT NULL,
                                    access_token TEXT,
                                    refresh_token TEXT,
                                    expiration_time_millis BIGINT,
                                    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                   CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+
+                                   CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                                   CONSTRAINT uq_user_provider UNIQUE (user_id, provider)
 );
