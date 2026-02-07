b36e64181269a4c28bb6f235822fc6fd1ac504c6
xenoisenegger <113588584+Xeen00@users.noreply.github.com>
2026-02-07 15:34:07 +0100

    GoogleCalendarApi: frontend: calendar-callback add component

---
+++ frontend/src/app/components/calendar-callback/calendar-callback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-callback',
+  imports: [],
+  templateUrl: './calendar-callback.component.html',
+  styleUrl: './calendar-callback.component.css'
+})
+export class CalendarCallbackComponent {
+
+}
