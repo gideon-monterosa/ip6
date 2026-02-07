cd20683c214e6d1ba987adb117a07be20d1bfb83
xenoisenegger <113588584+Xeen00@users.noreply.github.com>
2026-02-07 14:56:15 +0100

    GoogleCalendarApi: EventDto: remove lombok

--- backend/src/main/java/ch/fhnw/meeting/dto/calendar/EventDto.java
+++ backend/src/main/java/ch/fhnw/meeting/dto/calendar/EventDto.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class EventDto {
     private String id;
     private String title;
@@ -16,4 +11,52 @@ public class EventDto {
     private String start;
     private String end;
     private String link;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
