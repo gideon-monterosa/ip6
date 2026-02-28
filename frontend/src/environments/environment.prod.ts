565e79205585be84539b0650c59003804ef97e59
xenoisenegger <113588584+Xeen00@users.noreply.github.com>
2026-02-28 13:13:16 +0100

    deployment: add cicd:to run on github action

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com',
+  apiUrl: 'https://meetings.isenegger.dev/api',
+  firebaseConfig: {
+    apiKey: "__FIREBASE_API_KEY__",
+    authDomain: "__FIREBASE_AUTH_DOMAIN__",
+    projectId: "__FIREBASE_PROJECT_ID__",
+    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
+    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+    appId: "__FIREBASE_APP_ID__",
+    measurementId: "__FIREBASE_MEASUREMENT_ID__"
+  },
+  vapidKey: "__FIREBASE_VAPID_KEY__"
 };
